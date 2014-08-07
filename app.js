8fedfe27f5c1ddad39ac204c20b382e67cc0283c
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-07 14:29:50 -0400

    Full Job

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 $(document).ready(function() {
 
-alert("hello world");
 
+	$("#input")
+	.fadeIn();
+
+
+	$("button")
+	.fadeIn()
+	.on("click", function() {
+		var number = $("#input").val();
+		var body = $("body");
+
+		$("#holder").fadeOut("slow", function() {
+
+			$("#holder").empty();
+
+			for(var i=1; i <= number; i++) {
+
+				if((i%3 === 0) && (i%5 === 0)){
+					$("#holder").append("<div>fizz buzz</div>");
+				} else if (i%3 === 0) {
+					$("#holder").append("<div>fizz</div>");	
+				} else if (i%5 === 0) {
+					$("#holder").append("<div>buzz</div>");
+				} else {
+					$("#holder").append("<div>" + i + "</div>");
+				}
+
+			}
+
+			$("#holder").fadeIn("slow");
+		
+		});
+		
+
+	});
+
+
+	$(document)
+	.on("keydown", function(e) {
+		if(e.which === 13) {
+			$("button").click();
+		}
+	});
 
 
 });
