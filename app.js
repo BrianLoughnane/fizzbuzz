b96c53c37637165b6d63b91de502294f196aaa88
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-11 09:47:17 -0400

    Conflict Resolve

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
-	var fizzBuzz = function(number) {
+	var answer = prompt("Please enter a number to fizz buzz up to");
 
-		$("#holder").fadeOut();
-		$("#holder").empty();
+	$("#input")
+	.fadeIn();
 
+<<<<<<< HEAD
 		for(var i=1; i <= number; i++) {
 			if((i%3===0)&&(i%5===0)){
 				$("#holder").append("<div>fizz buzz</div>");
@@ -49,47 +50,39 @@ $(document).ready(function() {
 	};
 
 	checker();
+=======
+>>>>>>> parent of e8c036a... Refactor using functions
 
+	$("button")
+	.fadeIn()
+	.on("click", function() {
+		var number = $("#input").val();
+		var body = $("body");
 
+		$("#holder").fadeOut("slow", function() {
 
+			$("#holder").empty();
 
+			for(var i=1; i <= number; i++) {
 
+				if((i%3 === 0) && (i%5 === 0)){
+					$("#holder").append("<div>fizz buzz</div>");
+				} else if (i%3 === 0) {
+					$("#holder").append("<div>fizz</div>");	
+				} else if (i%5 === 0) {
+					$("#holder").append("<div>buzz</div>");
+				} else {
+					$("#holder").append("<div>" + i + "</div>");
+				}
 
+			}
 
-	// $("#input")
-	// .fadeIn();
-
-
-	// $("button")
-	// .fadeIn()
-	// .on("click", function() {
-	// 	var number = $("#input").val();
-	// 	var body = $("body");
-
-	// 	$("#holder").fadeOut("slow", function() {
-
-	// 		$("#holder").empty();
-
-	// 		for(var i=1; i <= number; i++) {
-
-	// 			if((i%3 === 0) && (i%5 === 0)){
-	// 				$("#holder").append("<div>fizz buzz</div>");
-	// 			} else if (i%3 === 0) {
-	// 				$("#holder").append("<div>fizz</div>");	
-	// 			} else if (i%5 === 0) {
-	// 				$("#holder").append("<div>buzz</div>");
-	// 			} else {
-	// 				$("#holder").append("<div>" + i + "</div>");
-	// 			}
-
-	// 		}
-
-	// 		$("#holder").fadeIn("slow");
+			$("#holder").fadeIn("slow");
 		
-	// 	});
+		});
 		
 
-	// });
+	});
 
 
 	$(document)
