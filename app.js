b25f6544373d8651dd7cdc0cf4b7baae2c94ea75
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-11 11:06:25 -0400

    New Refactor

--- app.js
+++ app.js
@@ -1,95 +1,110 @@
 $(document).ready(function() {
 
-	var fizzBuzz = function(number) {
+	// var fizzBuzz = function(number) {
 
-		$("#holder").fadeOut();
-		$("#holder").empty();
+	// 	$("#holder").fadeOut("slow");
+	// 	$("#holder").empty();
 
-		for(var i=1; i <= number; i++) {
-			if((i%3===0)&&(i%5===0)){
-				$("#holder").append("<div>fizz buzz</div>");
-			} else if(i%3===0) {
-				$("#holder").append("<div>fizz</div>");
-			} else if(i%5===0) {
-				$("#holder").append("<div>buzz</div>");
-			} else {
-				$("#holder").append("<div>"+i+"</div>");
+	// 	for(var i=1; i <= number; i++) {
+	// 		if((i%3===0)&&(i%5===0)){
+	// 			$("#holder").append("<div>fizz buzz</div>");
+	// 		} else if(i%3===0) {
+	// 			$("#holder").append("<div>fizz</div>");
+	// 		} else if(i%5===0) {
+	// 			$("#holder").append("<div>buzz</div>");
+	// 		} else {
+	// 			$("#holder").append("<div>"+i+"</div>");
+	// 		}
+	// 	}
+
+
+	// 	$("#holder").fadeIn("slow", checker);
+		
+
+
+	// };	
+
+
+	// var checker = function() {
+	// 	var answer = $("#input").val();
+	// 	answer = +answer;
+
+	// 	if(answer === NaN) {
+	// 		answer = alert(answer + " is not a number.  Please enter a number.")
+	// 	} else if(answer % 1 !== 0) {
+	// 		answer = alert(answer + " is a decimal.  Please enter a whole number.")
+	// 	} else if(answer < 1) {
+	// 		answer = alert(answer + " is less than 1.  Please enter a number 1 or greater.")
+	// 	} else {
+	// 		fizzBuzz(answer);
+
+	// 	}
+	// };
+
+	// checker();
+
+
+
+
+	// Old version
+
+
+
+
+	var fizzbuzz = function() {
+		var number = $("#input").val();
+
+		$("#holder").fadeOut("slow", function() {
+
+			$("#holder").empty();
+
+			for(var i=1; i <= number; i++) {
+
+				if((i%3 === 0) && (i%5 === 0)){
+					$("#holder").append("<div>fizz buzz</div>");
+				} else if (i%3 === 0) {
+					$("#holder").append("<div>fizz</div>");	
+				} else if (i%5 === 0) {
+					$("#holder").append("<div>buzz</div>");
+				} else {
+					$("#holder").append("<div>" + i + "</div>");
+				}
+
 			}
-		}
 
-		// $("#holder").fadeIn("slow").delay(1600, function() {
-		// 	checker();
-		// });
-
-		// $("#holder").fadeIn("slow", function() {
-		// 	checker();
-		// });
+			$("#holder").fadeIn("slow");
 		
-		$("#holder").fadeIn("slow", checker);
+		});
 		
 
-
-	};	
+	};
+	
 
 
 	var checker = function() {
-		var answer = prompt("Please enter a number to fizz buzz up to");
-		answer = +answer;
+		var answer = $("#input").val();
+		// answer = +answer;
 
-		if(answer === NaN) {
-			answer = prompt(answer + " is not a number.  Please enter a number.")
+		if(isNaN(answer)) {
+			answer = alert(answer + " is not a number.  Please enter a number.")
 		} else if(answer % 1 !== 0) {
-			answer = prompt(answer + " is a decimal.  Please enter a whole number.")
+			answer = alert(answer + " is a decimal.  Please enter a whole number.")
 		} else if(answer < 1) {
-			answer = prompt(answer + " is less than 1.  Please enter a number 1 or greater.")
+			answer = alert(answer + " is less than 1.  Please enter a number 1 or greater.")
 		} else {
-			fizzBuzz(answer);
+			fizzbuzz(answer);
 
 		}
 	};
 
-	checker();
+
+	$("#input")
+	.fadeIn();
 
 
-
-
-
-
-
-	// $("#input")
-	// .fadeIn();
-
-
-	// $("button")
-	// .fadeIn()
-	// .on("click", function() {
-	// 	var number = $("#input").val();
-	// 	var body = $("body");
-
-	// 	$("#holder").fadeOut("slow", function() {
-
-	// 		$("#holder").empty();
-
-	// 		for(var i=1; i <= number; i++) {
-
-	// 			if((i%3 === 0) && (i%5 === 0)){
-	// 				$("#holder").append("<div>fizz buzz</div>");
-	// 			} else if (i%3 === 0) {
-	// 				$("#holder").append("<div>fizz</div>");	
-	// 			} else if (i%5 === 0) {
-	// 				$("#holder").append("<div>buzz</div>");
-	// 			} else {
-	// 				$("#holder").append("<div>" + i + "</div>");
-	// 			}
-
-	// 		}
-
-	// 		$("#holder").fadeIn("slow");
-		
-	// 	});
-		
-
-	// });
+	$("button")
+	.fadeIn()
+	.on("click", checker);
 
 
 	$(document)
