03eec134a8af755aca3606e7182bb64dd8afc15b
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-11 10:29:52 -0400

    Revert "Refactor using functions"
    This reverts commit e8c036ad82c7d4d3beba2f2351c34b22f70c962d.

--- app.js
+++ app.js
@@ -1,88 +1,41 @@
 $(document).ready(function() {
 
-	var fizzBuzz = function(number) {
+	var answer = prompt("Please enter a number to fizz buzz up to");
 
-		$("#holder").fadeOut();
-		$("#holder").empty();
+	$("#input")
+	.fadeIn();
 
-		for(var i=1; i <= number; i++) {
-			if((i%3===0)&&(i%5===0)){
-				$("#holder").append("<div>fizz buzz</div>");
-			} else if(i%3===0) {
-				$("#holder").append("<div>fizz</div>");
-			} else if(i%5===0) {
-				$("#holder").append("<div>buzz</div>");
-			} else {
-				$("#holder").append("<div>"+i+"</div>");
+
+	$("button")
+	.fadeIn()
+	.on("click", function() {
+		var number = $("#input").val();
+		var body = $("body");
+
+		$("#holder").fadeOut("slow", function() {
+
+			$("#holder").empty();
+
+			for(var i=1; i <= number; i++) {
+
+				if((i%3 === 0) && (i%5 === 0)){
+					$("#holder").append("<div>fizz buzz</div>");
+				} else if (i%3 === 0) {
+					$("#holder").append("<div>fizz</div>");	
+				} else if (i%5 === 0) {
+					$("#holder").append("<div>buzz</div>");
+				} else {
+					$("#holder").append("<div>" + i + "</div>");
+				}
+
 			}
-		}
 
-		$("#holder").fadeIn("slow").delay(1600, function() {
-			checker();
+			$("#holder").fadeIn("slow");
+		
 		});
-
-
-	};	
-
-
-	var checker = function() {
-		var answer = prompt("Please enter a number to fizz buzz up to");
-		answer = +answer;
-
-		if(answer === NaN) {
-			answer = prompt(answer + " is not a number.  Please enter a number.")
-		} else if(answer % 1 !== 0) {
-			answer = prompt(answer + " is a decimal.  Please enter a whole number.")
-		} else if(answer < 1) {
-			answer = prompt(answer + " is less than 1.  Please enter a number 1 or greater.")
-		} else {
-			fizzBuzz(answer);
-
-		}
-	};
-
-	checker();
-
-
-
-
-
-
-
-	// $("#input")
-	// .fadeIn();
-
-
-	// $("button")
-	// .fadeIn()
-	// .on("click", function() {
-	// 	var number = $("#input").val();
-	// 	var body = $("body");
-
-	// 	$("#holder").fadeOut("slow", function() {
-
-	// 		$("#holder").empty();
-
-	// 		for(var i=1; i <= number; i++) {
-
-	// 			if((i%3 === 0) && (i%5 === 0)){
-	// 				$("#holder").append("<div>fizz buzz</div>");
-	// 			} else if (i%3 === 0) {
-	// 				$("#holder").append("<div>fizz</div>");	
-	// 			} else if (i%5 === 0) {
-	// 				$("#holder").append("<div>buzz</div>");
-	// 			} else {
-	// 				$("#holder").append("<div>" + i + "</div>");
-	// 			}
-
-	// 		}
-
-	// 		$("#holder").fadeIn("slow");
-		
-	// 	});
 		
 
-	// });
+	});
 
 
 	$(document)
